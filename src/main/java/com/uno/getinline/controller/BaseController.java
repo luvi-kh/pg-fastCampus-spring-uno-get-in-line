144e69f24b7232452258f3994b6f22ccc3830ee5
luvi-kh <nalsengom@daum.net>
2024-07-07 11:24:35 +0900

    2)화면진입점 화면 설정하는법 - thymeleaf dependency 추가시
    resources/ templates/ 아래 index.html이 있으면 이를 화면진입점으로 잡음,
    화면진입점잡기위해 thymeleaf dependency 사용하는 아이러니

--- src/main/java/com/uno/getinline/controller/BaseController.java
+++ src/main/java/com/uno/getinline/controller/BaseController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BaseController {
 
-//    @GetMapping("/")
-//    public String root(){
-//        return "index";
-//    }
+    @GetMapping("/")
+    public String root(){
+        return "index";
+    }
 }
