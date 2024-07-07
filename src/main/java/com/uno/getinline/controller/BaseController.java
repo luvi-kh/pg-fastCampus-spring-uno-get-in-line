2efe0ff3276771d865fa02939407ce4c33839ca8
luvi-kh <nalsengom@daum.net>
2024-07-07 21:21:55 +0900

    기본error page를 잡기 위한 설정 + ControllerAdvice사용
    @ControllerAdvice(basePackageClasses = BaseController.class)

--- src/main/java/com/uno/getinline/controller/BaseController.java
+++ src/main/java/com/uno/getinline/controller/BaseController.java
@@ -2,19 +2,22 @@ package com.uno.getinline.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@ControllerAdvice(basePackageClasses = BaseController.class)
 @Controller
-public class BaseController implements ErrorController {
+public class BaseController  {
 
     @GetMapping("/")
-    public String root(){
-        return "index";
+    public String root() throws Exception{
+        throw new Exception("테스트");
+//        return "index";
     }
 
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
-    }
+//    @RequestMapping("/error")
+//    public String error() {
+//        return "error";
+//    }
 }
