2e170237ac35f2fd87c116c174ffe0c593473414
luvi-kh <nalsengom@daum.net>
2024-07-08 21:10:11 +0900

    TestConstructor를 사용해서 상단에 세팅도 가능
    하지만 권장하진 않는다.

--- src/test/java/com/uno/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/uno/getinline/controller/BaseControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @AutoConfigureMockMvc
 @SpringBootTest
 class BaseControllerTest {
@@ -25,7 +27,7 @@ class BaseControllerTest {
     private final MockMvc mvc;
 
 
-    public BaseControllerTest(@Autowired MockMvc mvc) {
+    public BaseControllerTest(MockMvc mvc) {
         this.mvc = mvc;
     }
 
