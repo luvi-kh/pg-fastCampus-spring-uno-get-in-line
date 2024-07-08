545736e24c1b535ba21c65f64012b3b22e298d80
luvi-kh <nalsengom@daum.net>
2024-07-08 21:50:50 +0900

    APIPlaceControllerTest 생성

---
+++ src/test/java/com/uno/getinline/controller/api/APIPlaceControllerTest.java
@@ -0,0 +1,46 @@
+package com.uno.getinline.controller.api;
+
+import com.uno.getinline.constant.ErrorCode;
+import com.uno.getinline.constant.PlaceType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(APIPlaceController.class)
+class APIPlaceControllerTest {
+
+    private final MockMvc mvc;
+
+    public APIPlaceControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[API][GET] 장소 리스트 조회 - 장소 리스트 데이터를 담은 표준 API 출력")
+    @Test
+    void givenNothing_whenRequestingPlaces_thenReturnsPlacesInStandardResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/places"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data[0].placeType").value(PlaceType.COMMON.name()))
+                .andExpect(jsonPath("$.data[0].placeName").value("랄라배드민턴장"))
+                .andExpect(jsonPath("$.data[0].address").value("서울시 강남구 강남대로 1234"))
+                .andExpect(jsonPath("$.data[0].phoneNumber").value("010-1234-5678"))
+                .andExpect(jsonPath("$.data[0].capacity").value(30))
+                .andExpect(jsonPath("$.data[0].memo").value("신장개업"))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
+                .andExpect(jsonPath("$.message").value(ErrorCode.OK.getMessage()));
+    }
+}
