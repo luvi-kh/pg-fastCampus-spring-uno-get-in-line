6b5946ba4bbcf766057a06d4f05b6afc3204fd16
luvi-kh <nalsengom@daum.net>
2024-07-08 21:13:37 +0900

    @WebMvcTest 사용해서 BaseController 를 띄움

--- src/test/java/com/uno/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/uno/getinline/controller/BaseControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestConstructor;
@@ -18,16 +19,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(BaseController.class)
 class BaseControllerTest {
 
 //    @Autowired
     private final MockMvc mvc;
 
 
-    public BaseControllerTest(MockMvc mvc) {
+    public BaseControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
